930e8eb16ce6c2a9cd94f97ea2c7717e6183998f
unknown <scandiclamar@gmail.com>
2025-06-03 10:28:03 +0000

    adding backend url

--- sec_frontend/src/components/chatbox.tsx
+++ sec_frontend/src/components/chatbox.tsx
@@ -14,7 +14,7 @@ interface Message {
   content: string;
 }
 
-const BASE_URL = 'http://127.0.0.1:8000/api';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL + 'api';
 
 export const useChat = ({
   chartData,
